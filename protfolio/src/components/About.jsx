c064880662cde2d2e35f6471e288a4264adbe655
donjoo <donjorois@gmail.com>
2024-11-12 03:31:51 +0530

    hero,experience

--- protfolio/src/components/About.jsx
+++ protfolio/src/components/About.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import {Tilt} from 'react-tilt'
+import { motion}  from 'framer-motion';
+import {styles} from '../styles';
+import { services } from '../constants';
+import {fadeIn, textVariant }  from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+
+const ServiceCard = ({index, title, icon}) => {
+
+   return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div 
+      variants={fadeIn("right","spring",0.5 * index,0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px]
+        py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+ 
+        </div>
+      </motion.div>
+    </Tilt>
+
+   )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+  <>
+  <motion.div variants={textVariant()}>
+    <p className={styles.sectionSubText}>Introduction</p>
+    <h2 className={styles.sectionHeadText}>Overview</h2>
+  </motion.div>
+
+
+  <motion.p variants={fadeIn("","",0.1,1)} className='mt-4 text-secondary text-[17px]
+  max-w-3x1 leading-[30px]'>
+    Hi, I'm Don Jo Rois, a passionate full-stack developer
+    with a strong foundation in Python, Django, React, 
+    and PostgreSQL. I am continuously learning and 
+    evolving in the ever-changing world of technology. 
+    With a solid understanding of both frontend 
+    and backend development, I build scalable and 
+    efficient web applications.
+
+  I’ve worked on a variety of projects, including user m
+  anagement systems, to-do apps, and data analysis tools.
+  My expertise also includes creating intuitive user 
+  interfaces with React, managing backend operations with 
+  Django, and ensuring seamless database interactions with
+  PostgreSQL.
+  </motion.p>
+
+  <div className='mt-20 flex flex-wrap gap-10'>
+    {services.map((service,index) => (
+      <ServiceCard key={service.title} index={index} {...service}/>
+    ) )}
+
+  </div>
+  </>
   )
 }
 
-export default About
+export default SectionWrapper(About,"about")
--- protfolio/src/components/Navbar.jsx
+++ protfolio/src/components/Navbar.jsx
@@ -7,8 +7,13 @@ import { logo,menu,close  } from '../assets';
 
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("");
+  const [toggle,setToggle] = useState(false);
+
+
   return (
-    <nav className={`${styles.paddindx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/"
         className='flex items-center gap-2'onClick={() => {
@@ -17,7 +22,41 @@ const Navbar = () => {
         }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain"></img>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>DonJO &nbsp;<span className='sm:block hidden'>| Python Mastery</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>{navLinks.map((Link) => (
+          <li key={Link.id}
+          className ={`${
+            active == Link.title ? "text-white": "text-secondary"
+          } hover:text-white text-[18px] font-medium cursor-pointer`}
+          onClick ={() => 
+            setActive(Link.title)}>
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>
+        ))}</ul>
+
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img 
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)} > 
+          </img>
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`} >
+          <ul className='list-none flex justify-end items-start flex-col gap-4'>{navLinks.map((Link) => (
+          <li key={Link.id}
+          className ={`${
+            active == Link.title ? "text-white": "text-secondary"
+          } hover:text-white font-poppins font-medium cursor-pointer text-[16px]`}
+          onClick ={() => {
+            setToggle(!toggle)
+            setActive(Link.title)}}>
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>
+        ))}</ul>
+          </div>
+        </div>
       </div>
     </nav>
     
